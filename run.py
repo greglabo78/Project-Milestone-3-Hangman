deb81a1b3c792fa751e81f0993d558940340d93e
greglabo78 <gregoryobigodwin@yahoo.com>
2023-03-14 07:10:21 +0000

    Add Intial game structure with colors for text.

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from art import HANGMAN
+
+# Color codes to be used within the game
+ALERT = "\033[91m"
+RESPONSE = "\033[92m"
+TEXT = "\033[0m"
+HIGHLIGHT = "\033[7m"
+
+COLOURS = {
+    "red": "\033[31m",
+    "green": "\033[32m",
+    "yellow": "\033[33m",
+    "blue": "\033[34m",
+    "pink": "\033[95m",
+    "cyan": "\033[36m",
+    "purple": "\033[35m"
+}
