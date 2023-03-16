979cd5c0d0d498ba9a28a13eea19b2e105730c95
greglabo78 <gregoryobigodwin@yahoo.com>
2023-03-16 14:06:26 +0000

    Python code corrected

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import random
 
-
 # Color codes to be used within the game
 COLOURS = {
     "red": "\033[31m",
@@ -13,12 +12,14 @@ COLOURS = {
 }
 
 # Collection of words to be used in the game
-words = ['lagos', 'sweet', 'friend', 'japan', 'elephant', 'mountain', 'bicycle', 'taxi', 'fruit', 'bank']
-
+words = [
+    'lagos', 'sweet', 'friend', 'japan', 'elephant', 'mountain',
+    'bicycle', 'taxi', 'fruit', 'bank'
+]
 
 # List of hangman stages
 hangman_stages = [
-    """
+    r"""
     +---+
         |
         |
@@ -26,7 +27,7 @@ hangman_stages = [
         |
        ===
     """,
-    """
+    r"""
     +---+
     O   |
         |
@@ -34,7 +35,7 @@ hangman_stages = [
         |
        ===
     """,
-    """
+    r"""
     +---+
     O   |
     |   |
@@ -42,7 +43,7 @@ hangman_stages = [
         |
        ===
     """,
-    """
+    r"""
     +---+
     O   |
    /|   |
@@ -50,7 +51,7 @@ hangman_stages = [
         |
        ===
     """,
-    """
+    r"""
     +---+
     O   |
    /|\  |
@@ -58,7 +59,7 @@ hangman_stages = [
         |
        ===
     """,
-    """
+    r"""
     +---+
     O   |
    /|\  |
@@ -66,7 +67,7 @@ hangman_stages = [
         |
        ===
     """,
-    """
+    r"""
     +---+
     O   |
    /|\  |
@@ -93,48 +94,46 @@ def initiate_game(word):
     guessed_max = 6
     num_guesses = 0
     hangman_stage = 0
-    
-    # After initiating variables the game will print out 
-    
-    
+
     return guessed_letters, guessed_max, num_guesses, hangman_stage
 
 
 def guess_word(word):
     """
-    This function will print welcome statements 
+    This function will print welcome statements
     run the main function of the game
     """
     print(COLOURS["pink"] + "Welcome to the Hangman game!")
     print("The word has", COLOURS["cyan"], len(word), "letters.")
     print(" ".join("_" * len(word)))
-    
-    guessed_letters, guessed_max, num_guesses, hangman_stage = initiate_game(word)
+
+    guessed_letters, guessed_max, num_guesses, hangman_stage = \
+        initiate_game(word)
     game_over = False
-    
+
     # loop until game is over
     while not game_over:
         guess = input("Guess a letter:\n").lower()
-        
+
         # Check if guess has already been made
         if guess in guessed_letters:
             print("You already guessed that letter")
         else:
             guessed_letters.append(guess)
-        
+
         if guess in word:
             print("Correct!")
         else:
             print(f"{COLOURS['red']} That is not the right option!")
             num_guesses += 1
             hangman_stage += 1
-        
+
         for letter in word:
             if letter in guessed_letters:
                 print(letter, end=" ")
             else:
                 print("_", end=" ")
-        
+
         print()
 
         print(hangman_stages[hangman_stage])
@@ -150,9 +149,9 @@ def guess_word(word):
 
 def play_again():
     """
-    This functions keeps the game running without 
-    having to restart the program eanbling user
-    to have a continuos gaming experience
+    This function keeps the game running without
+    having to restart the program, enabling the user
+    to have a continuous gaming experience
     """
     while True:
         choice = input("Do you want to play again? (y/n): ")
@@ -162,7 +161,6 @@ def play_again():
             raise SystemExit("Game Over..\n")
         else:
             print("Invalid choice. Please enter 'y' or 'n'.")
-
 
 # Play Game
 
